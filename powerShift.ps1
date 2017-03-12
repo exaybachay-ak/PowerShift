16a05143e3f63586fb53dec42dcc38a6ca4ccc25
Jacob Kelley <exaybachay.ak@gmail.com>
2017-03-12 00:27:56 -0900

    Update powerShift.ps1
    Changed to PDF filetype and added info about MSXML.  Also cleaned some code up.

--- powerShift.ps1
+++ powerShift.ps1
@@ -3,7 +3,6 @@ param (
     [Parameter(Mandatory=$true)][string]$OutFile
 )
 
-
 <#######################################################################
 shift the file
 #######################################################################>
@@ -15,22 +14,28 @@ shift the file
 ####----> make array to store new shifted file data
 $shiftarrlist = New-Object System.Collections.Generic.List[System.String]
 
-# add PDF header bytes to variables to prep for adding to shiftarray
-# PDF Signature is 25 50 44 46, which is 19 32 2c 2e in bytes
-[byte[]] $header = 0x19,0x32,0x2C,0x2E
-
-<#
-25 is byte 00010101, or hex 19
-#>
+# add MSXML header bytes to variables to prep for adding to shiftarray
+[byte[]] $header = 0x37,0x80,0x68,0x70
+[byte[]] $footer = 0x10,0x37,0x37,0x69,0x79,0x70
 
 <#
 possible formats to use:
 
-#### DOC
-[byte[]] $header = "0xD0","0xCF","0x11","0xE0","0xA1","0xB1","0x1A","0xE1"
-[byte[]] $footer = "0x57","0x6F","0x72","0x64","0x2E","0x44","0x6F","0x63","0x75","0x6D","0x65","0x6E","0x74","0x2E"
+	#### MS OFFICE XML (docx, xlsx, pptx, etc...)
+	# MS Office XML hex signature is 50 4B 03 04 14 00 06 00, which is 80 75 3 4 20 0 6 0 in bytes
+	[byte[]] $header = 0x80,0x7,0x03,0x04,0x20,0x00,0x06,0x00
+	[byte[]] $footer = 0x50,0x4b,0x05,0x06,0x00,0x00,0x00,0x00,0x0F,0x00,0x0F,0x00,0xEE,0x03,0x00,0x00,0xF3,0x4C,0x00,0x00,0x00,0x00
 
-#### HTML
+	Actual footer from MS XLSX file for reference
+	80 75 5 6 0 0 0 0 15 0 15 0 238 3 0 0 243 76 0 0 0 0
+
+
+	# PDF hex Signature is 25 50 44 46, which is 19 32 2c 2e in bytes
+	[byte[]] $header = 0x37,0x80,0x68,0x70
+	[byte[]] $footer = 0x10,0x37,0x37,0x69,0x79,0x70
+
+	Actual footer from PDF file for reference
+	10 37 37 69 79 70
 
 #>
 
@@ -43,11 +48,11 @@ foreach($byte in $inbytes){
 	$shiftbyte = $byte -bxor 1
 	[void]$shiftarrlist.Add($shiftbyte)
 }
-<#
-foreach($f in $footerbytes){
-	[void]$shiftarrlist.Add($f)
+
+foreach($c in $footer){
+	[void]$shiftarrlist.Add($c)
 }
-#>
+
 $shiftarr = $shiftarrlist.ToArray()
 
 New-Item $outfile -type file | out-null
@@ -63,7 +68,7 @@ extract the file
 $filereadStream = [System.IO.File]::Open($outfile, [System.IO.FileMode]::Open, [System.IO.FileAccess]::Read, [System.IO.FileShare]::ReadWrite)
 $reader = New-Object System.IO.BinaryReader($filereadStream)
 $reader.BaseStream.Position = 4;
-$data = $reader.ReadBytes($shiftarr.length)
+$data = $reader.ReadBytes($shiftarr.length-10)
 $reader.close()
 
 
@@ -77,7 +82,6 @@ foreach($outbyte in $data){
 
 $unshiftarr = $unshiftarrlist.ToArray()
 
-#write-host $inbytes
-#write-host $unshiftarr
+write-host $unshiftarr
 
 [IO.File]::WriteAllBytes("test2.jpg", $unshiftarr)
